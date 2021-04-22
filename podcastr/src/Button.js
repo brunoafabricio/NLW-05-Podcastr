8227995eed5d3c98d52830715ba7f15aeb948fbf
Bruno <brunof.tech@gmail.com>
2021-04-21 23:10:59 -0300

    init

---
+++ podcastr/src/Button.js
@@ -0,0 +1,17 @@
+import Button from "./Button";
+
+function App(){
+  return(
+    <div>
+      <>
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+      </>
+    </div>
+  );
+}
+
+export

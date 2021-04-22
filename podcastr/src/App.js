c07e6d255e5ffd926a0ea550939fbc81b051ef9c
Bruno <brunof.tech@gmail.com>
2021-04-22 00:41:10 -0300

    dev-001

--- podcastr/src/App.js
+++ podcastr/src/App.js
@@ -4,11 +4,11 @@ function App(){
   return(
     <div>
       <>
-        <Button />
-        <Button />
-        <Button />
-        <Button />
-        <Button />
+        <Button>Button 1</Button>
+        <Button>Button 2</Button>
+        <Button>Button 3</Button>
+        <Button>Button 4</Button>
+        <Button>Button 5</Button>
       </>
     </div>
   );

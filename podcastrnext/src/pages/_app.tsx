3137f92d736342124f70608cef5b729faa53732e
Bruno <brunof.tech@gmail.com>
2021-04-25 18:35:35 -0300

    Day 5: Refactor Player Context

--- podcastrnext/src/pages/_app.tsx
+++ podcastrnext/src/pages/_app.tsx
@@ -5,39 +5,22 @@ import { Header } from "../components/Header";
 import { PlayerContext } from '../contexts/PlayerContext';
 
 import styles from '../styles/app.module.scss';
-import { useState } from 'react';
+import { PLayerContextProvider } from '../contexts/PlayerContext';
 
 
 function MyApp({ Component, pageProps }) {
-  const [episodeList, setEpisodeList] = useState([]);
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  function play(episode) {
-    setEpisodeList([episode]);
-    setCurrentEpisodeIndex(0);
-    setIsPlaying(true);
-  }
-
-  function togglePlay() {
-    setIsPlaying(!isPlaying);
-  }
-
-  function setPlayingState(state: boolean) {
-    setIsPlaying(state);
-  }
 
   return (
-    <PlayerContext.Provider value={{episodeList, currentEpisodeIndex, isPlaying, play, togglePlay, setPlayingState}}>
-    <div className={styles.wrapper}>
-     <main>
-      <Header />
-      <Component {...pageProps} />
-     </main>
-     <Player />
-    </div>
-    </PlayerContext.Provider>
-  )
+    <PLayerContextProvider>
+      <div className={styles.appWrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PLayerContextProvider>
+  );
 }
 
 export default MyApp
